c4886688731be255330858872e945af822f4d95e
Joel Nyonga <joel.nyonga@mymail.champlain.edu>
2026-02-09 14:17:40 -0500

    Adrenaline, Fight, Flight, Burnout Meters are functional
    Debug text displays current status of player as well as percentage values for each bar

--- Adrenaline/Assets/Scripts/Adrenaline.cs
+++ Adrenaline/Assets/Scripts/Adrenaline.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class Adrenaline : MonoBehaviour
 {
     [Header("References")]
     public Image adrenalineFillBar;
+    public TextMeshProUGUI debugAdrenaline;
     public Image fightFillBar;
+    public TextMeshProUGUI debugFight;
     public Image flightFillBar;
+    public TextMeshProUGUI debugFlight;
+    public Image fightBurnout;
+    public Image flightBurnout;
+    public TextMeshProUGUI debugStatus;
+
+    [Header("Controls")]
+    public InputActionAsset InputActions;
+
+    private InputAction m_activateFightAction;
+    private InputAction m_activateFlightAction;
 
     [Header("Adrenaline Settings")]
     [SerializeField]
@@ -40,6 +54,18 @@ public class Adrenaline : MonoBehaviour
     [SerializeField]
     public bool isFlightActive = false;
 
+    [Header("Burnout Bar Settings")]
+    [SerializeField]
+    public float maxBurnout = 100f;
+    [SerializeField]
+    public float currentBurnout = 0f;
+    [SerializeField]
+    public bool isBurnoutActive = false;
+
+    private float drainDuration = 30f;
+    private float burnoutDuration = 15f;
+    private float drainTimer = 0f;
+
     public void Start()
     {
         currentAdrenaline = maxAdrenaline;
@@ -47,11 +73,34 @@ public class Adrenaline : MonoBehaviour
     public void Update()
     {
         AdrenalineDrain();
+
+        FightMechanic();
+
+        FlightMechanic();
+
+        Burnout();
+
+        DebugText();
+
+    }
+    private void Awake()
+    {
+        m_activateFightAction = InputSystem.actions.FindAction("ActivateFight");
+        m_activateFlightAction = InputSystem.actions.FindAction("ActivateFlight");
+    }
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+
     }
     public void AdrenalineDrain()
     {
         if (inCombat == false && currentAdrenaline > minAdrenaline)
-        {
+        { 
             currentAdrenaline -= (Time.deltaTime * adrenalineDrainRateMulti);
         }
 
@@ -75,13 +124,97 @@ public class Adrenaline : MonoBehaviour
         }
     }
 
-    public void FightMechanic(int )
+    public void FightMechanic()
     {
         FillBarUpdate(fightFillBar, currentFight, maxFight);
+
+        if(m_activateFightAction.WasPressedThisFrame() && currentFight == maxFight && !isFlightActive && !isBurnoutActive)
+        {
+            isFightActive = true;
+        }
+
+        if (isFightActive)
+        {
+            drainTimer += Time.deltaTime;
+            currentFight = Mathf.Lerp(maxFight, 0f, drainTimer / drainDuration);
+
+            if(drainTimer >= drainDuration)
+            {
+                currentFight = 0;
+                isFightActive = false;
+                drainTimer = 0;
+                isBurnoutActive = true;
+            }
+        }
+        
     }
 
     public void FlightMechanic()
     {
         FillBarUpdate(flightFillBar, currentFlight, maxFlight);
+
+        if (m_activateFlightAction.WasPressedThisFrame() && currentFlight == maxFlight && !isFightActive && !isBurnoutActive)
+        {
+            isFlightActive = true;
+        }
+
+        if (isFlightActive)
+        {
+            drainTimer += Time.deltaTime;
+            currentFlight = Mathf.Lerp(maxFlight, 0f, drainTimer / drainDuration);
+
+            if (drainTimer >= drainDuration)
+            {
+                currentFlight = 0;
+                isFlightActive = false;
+                drainTimer = 0;
+                isBurnoutActive = true;
+            }
+        }
+    }
+    public void Burnout()
+    {
+        FillBarUpdate(flightBurnout, currentBurnout, maxBurnout);
+        FillBarUpdate(fightBurnout, currentBurnout, maxBurnout);
+
+        if (isBurnoutActive)
+        {
+            drainTimer += Time.deltaTime;
+            currentBurnout = Mathf.Lerp(maxBurnout, 0f, drainTimer / burnoutDuration);
+
+            if (drainTimer >= drainDuration)
+            {
+                currentBurnout = 0;
+                isBurnoutActive = false;
+                drainTimer = 0;
+            }
+        }
+        else
+        {
+            currentBurnout = 0;
+        }
+    }
+    public void DebugText()
+    {
+        debugAdrenaline.text = $"{(currentAdrenaline/maxAdrenaline) * 100:F0}%";
+        debugFight.text = $"{(currentFight/maxFight) * 100:F0}%";
+        debugFlight.text = $"{(currentFlight/maxFlight) * 100:F0}%";
+
+        if(isFightActive)
+        {
+            debugStatus.text = "Status: Fight Mode Active";
+        }
+        else if(isFlightActive)
+        {
+            debugStatus.text = "Status: Flight Mode Active";
+        }
+        else if(isBurnoutActive)
+        {
+            debugStatus.text = "Status: Burnout Mode Active";
+        }
+        else
+        {
+            debugStatus.text = "Status: Neutral";
+        }
     }
 }
